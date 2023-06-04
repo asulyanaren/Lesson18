0cd221602fb83108cdfd75d180bb9cd4e84244ae
aren1994asulyan <aren1994asulyan@gmail.com>
2023-06-04 18:33:29 +0400

    Add project files.

---
+++ Lesson18/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 50; i < 70; i++)
+            {
+                for (int j = i; j >= 1;) {
+                   
+                    if (j % 2 == 0)
+                    {
+                        j = j / 2;
+                     }
+                    else
+                    {
+                        j = (j *3 +1) / 2;
+                     }
+                    if (j==1) {
+                        Console.WriteLine(j);
+                        break;
+                    }
+                 }
+               
+            }
+        }
+    }
+}
